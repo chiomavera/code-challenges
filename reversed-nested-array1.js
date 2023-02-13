187c8e7ccd168a2052beccfbb7fb966d40ad9caf
Chioma Vera Nkanmuo <chiomaverankanmuo@gmail.com>
2023-02-13 13:59:05 +0100

    Create reversed-nested-array1.js

---
+++ reversed-nested-array1.js
@@ -0,0 +1,14 @@
+// creating a nested array in reverse order
+
+let extctr = 1;
+for (let i = 0; i < 3; i++) {
+  let littleArr = [];
+  for (let j = 0; j < 3; j++) {
+    littleArr.push(extctr);
+    extctr++;
+    littleArr.reverse()
+  }
+  bigarr.push(littleArr);
+}
+bigarr.reverse()
+console.log(bigarr);
