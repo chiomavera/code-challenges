6480e7bdb2386cfec847c65fd7880e765750ede5
Chioma Vera Nkanmuo <chiomaverankanmuo@gmail.com>
2023-02-14 14:13:47 +0100

    Update factorialize.js

--- factorialize.js
+++ factorialize.js
@@ -1,8 +1,27 @@
+//using recursive function
+
 function factorialize(num) {
 if (num === 0) {
   return 1;
 }
 return num * factorialize(num-1)
+}
+
+let result = factorialize(5);
+console.log(result)
+
+
+//or
+
+// using for loop
+
+function factorialize(num) {
+  let output = 1;
+  for (let i = 1; i <= num; i++) {
+    
+  output *= i;
+}
+  return output;
 }
 
 let result = factorialize(5);
