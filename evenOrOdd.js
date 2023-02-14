bd9193e563bb7565022f87fabb6858ecfe6d279f
Chioma Vera Nkanmuo <chiomaverankanmuo@gmail.com>
2023-02-14 15:23:21 +0100

    Create evenOrOdd.js

---
+++ evenOrOdd.js
@@ -0,0 +1,7 @@
+// A function that accepts a number and checks whether it is even or odd
+
+let evenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd"
+  
+let result = evenOrOdd(5);
+
+console.log(result)
